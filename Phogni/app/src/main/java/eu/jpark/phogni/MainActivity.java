76f0ec3feae9c725258d3799ce3b4a4960ed252a
jpark <heyheygo77@gmail.com>
2016-12-25 22:21:11 +0900

    Phogni 프로젝트 생성

---
+++ Phogni/app/src/main/java/eu/jpark/phogni/MainActivity.java
@@ -0,0 +1,13 @@
+package eu.jpark.phogni;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
